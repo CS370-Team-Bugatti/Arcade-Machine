00eceafc8b2f5a177a7f84ba56a8d908cad19c7d
BraedenSmith <57156456+BraedenSmith@users.noreply.github.com>
2019-12-03 23:21:56 -0700

    Removed misplaced test print

--- Communicator.py
+++ Communicator.py
@@ -4,7 +4,6 @@ from gpiozero import MotionSensor
 def sense():
     pir = MotionSensor(4)
     if (pir.wait_for_motion()):
-        print ("Motion Detected") #Test print
         return 1
     
 sense()
