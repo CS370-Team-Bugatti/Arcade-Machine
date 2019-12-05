49bde81daa8ce54f9d6d24000a3023459d570b13
jwelch31 <46934297+jwelch31@users.noreply.github.com>
2019-12-05 10:07:31 -0700

    Update Game2.h

--- Game2.h
+++ Game2.h
@@ -10,11 +10,13 @@
 
 class ReflexGame{
 	public:
-		/*** methods ***/
-		//ctor
-		ReflexGame();
 		//returns average reflex in seconds
 		int runGame();
+		//ctor
+		ReflexGame();
+	private:
+		/*** methods ***/
+		
 		void PrintScore();
 		void printSelection();
 		bool validate(int);
@@ -22,14 +24,13 @@ class ReflexGame{
 		void printReactors();
 		void initializeTimeChange(bool&, int&);
 		void changeReactors();
+		void generateReaction();
 	
 		/*** variables ***/
 		//score is average reflex in seconds
 		int score;
 		//difficulty level (0=easy, 1=medium, 2=hard)
 		int difficulty;
-		//boolean for whether or not a reaction is expected
-		bool reaction;
 		std::string difficultyStr;
 		std::string warning;
 		int changeTimeMS;
@@ -37,6 +38,12 @@ class ReflexGame{
 		std::vector<int> reactors;
 		int vectorMax;
 		std::random_device rd;
+		int reaction;
+		double speed;
+		double minSpeed;	
+		bool speedDecr;
+		double multiplier;
+		bool newThread;
 };//end of format class
 
 #endif
